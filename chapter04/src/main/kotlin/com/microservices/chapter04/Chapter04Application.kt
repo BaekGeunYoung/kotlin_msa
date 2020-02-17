64df4bb73dc81973d64b019428e81ec48ac10bea
BaekGeunYoung <dvmflstm@naver.com>
2020-02-18 00:19:10 +0900

    initial commit

---
+++ chapter04/src/main/kotlin/com/microservices/chapter04/Chapter04Application.kt
@@ -0,0 +1,11 @@
+package com.microservices.chapter04
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Chapter04Application
+
+fun main(args: Array<String>) {
+    runApplication<Chapter04Application>(*args)
+}
