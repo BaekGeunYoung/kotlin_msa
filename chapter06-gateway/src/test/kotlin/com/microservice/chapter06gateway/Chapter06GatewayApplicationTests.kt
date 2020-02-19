64c5e451f84b68cca4dcde3d28aa683dd82efaf5
BaekGeunYoung <dvmflstm@naver.com>
2020-02-19 23:59:41 +0900

    실습용 gateway server 생성

---
+++ chapter06-gateway/src/test/kotlin/com/microservice/chapter06gateway/Chapter06GatewayApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.microservice.chapter06gateway
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Chapter06GatewayApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
