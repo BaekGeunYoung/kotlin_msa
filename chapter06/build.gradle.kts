e7061ec94002c6f973059683bc1af8e0b6c765ae
BaekGeunYoung <dvmflstm@naver.com>
2020-02-20 00:00:18 +0900

    microservice에 actuactor를 적용해 health check 기능 추가

--- chapter06/build.gradle.kts
+++ chapter06/build.gradle.kts
@@ -20,6 +20,8 @@ extra["springCloudVersion"] = "Hoxton.SR1"
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.springframework.cloud:spring-cloud-starter-config")
